8a1a81efca4e059568867ec81a5f1361dea00792
Zaixing <zaixing.mao@cern.ch>
2015-06-25 16:24:19 -0500

    config changes

--- NtupleTools/test/submit_TT.py
+++ NtupleTools/test/submit_TT.py
@@ -3,25 +3,26 @@ import os
 
 
 skimCuts = {"ID": "object.tauID(\\\"decayModeFinding\\\") > 0.5 || object.tauID(\\\"decayModeFindingNewDMs\\\") > 0.5",
-            "anti-Ele": "object.tauID(\\\"againstElectronVLooseMVA5\\\")>0.5",
-            "anti-Mu": "object.tauID(\\\"againstMuonLoose3\\\")>0.5",
-            "iso": "object.tauID(\\\"byCombinedIsolationDeltaBetaCorrRaw3Hits\\\")<10.0"
+            "Pt": "object.pt() > 45",
+#             "anti-Ele": "object.tauID(\\\"againstElectronVLooseMVA5\\\")>0.5",
+#             "anti-Mu": "object.tauID(\\\"againstMuonLoose3\\\")>0.5",
+#             "iso": "object.tauID(\\\"byCombinedIsolationDeltaBetaCorrRaw3Hits\\\")<10.0"
             }
 
 
-cmd = "./make_ntuples_cfg.py maxEvents=1000 outputFile=myTestFile.root useMiniAOD=1 inputFiles=file:/hdfs/store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU30bx50_PHYS14_25_V1-v1/00000/48D2DDEC-1281-E411-BF3C-002590AC4FEA.root channels=tt isMC=1 nExtraJets=8 svFit=1 "
+cmd = "./make_ntuples_cfg.py eventsToProcess=1:2267:226635  outputFile=myTestFile.root useMiniAOD=1 inputFiles=file:///hdfs/store/mc/Phys14DR/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v2/00000/66E71D9C-9F77-E411-BFE9-00266CF9B184.root channels=tt isMC=1 nExtraJets=6 svFit=0 "
+# cmd = "./make_ntuples_cfg.py maxEvents=1000  outputFile=myTestFile.root useMiniAOD=1 inputFiles=file:///hdfs/store/mc/Phys14DR/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v2/00000/66E71D9C-9F77-E411-BFE9-00266CF9B184.root channels=tt isMC=1 nExtraJets=6 svFit=0 "
 
 cuts = "skimCuts=\""
 for ikey in skimCuts.keys():
     cuts+= "%s," %(skimCuts[ikey].replace("object", "daughter(0)"))
     cuts+= "%s," %(skimCuts[ikey].replace("object", "daughter(1)"))
 cuts = cuts[0: len(cuts)-1] + "\""
-
+cmd += cuts
 tempFile = "do_test.sh"
 outFile = "do.sh"
-cmd = "submit_job.py MiniAOD_Test make_ntuples_cfg.py channels=\"tt\" isMC=1 nExtraJets=6 svFit=1"
-cmd += " --campaign-tag=\"Phys14DR*30*50*\" --das-replace-tuple=$fsa/MetaData/tuples/MiniAOD-13TeV.json --samples \"TTJet*\" -o %s" %tempFile
-print cmd
+cmd = "submit_job.py SYNC make_ntuples_cfg.py channels=\"tt\" isMC=1 useMiniAOD=1 nExtraJets=6 svFit=0"
+cmd += " --campaign-tag=\"Phys14DR-PU20bx25*\" --das-replace-tuple=$fsa/MetaData/tuples/MiniAOD-13TeV.json --samples \"VBFHtt*\" -o %s" %tempFile
 os.system(cmd)
 
 
@@ -38,8 +39,4 @@ for i in range(0, len(lines)):
     currentLine += '\n'
     print currentLine
     output.writelines(currentLine)
-
 output.close()
-#cmd += "\" --campaign-tag=\"Phys14DR-PU20bx25_tsg_PHYS14_25_V*\" --das-replace-tuple=$fsa/MetaData/tuples/MiniAOD-13TeV.json --samples \"VBF*\" -o do_test.sh"
-#os.system(cmd)
-#print cmd
