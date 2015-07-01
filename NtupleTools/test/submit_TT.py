a77873d5254a0a21d4da3175040119e08cb0c1f0
Zaixing <zaixing.mao@cern.ch>
2015-07-01 13:59:21 -0500

    config changes

--- NtupleTools/test/submit_TT.py
+++ NtupleTools/test/submit_TT.py
@@ -10,7 +10,7 @@ skimCuts = {"ID": "object.tauID(\\\"decayModeFinding\\\") > 0.5 || object.tauID(
             }
 
 
-cmd = "./make_ntuples_cfg.py eventsToProcess=1:2267:226635  outputFile=myTestFile.root useMiniAOD=1 inputFiles=file:///hdfs/store/mc/Phys14DR/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v2/00000/66E71D9C-9F77-E411-BFE9-00266CF9B184.root channels=tt isMC=1 nExtraJets=6 svFit=0 "
+cmd = "./make_ntuples_cfg.py eventsToProcess=1:1718:171763  outputFile=myTestFile.root useMiniAOD=1 inputFiles=file:///hdfs/store/mc/Phys14DR/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v2/10000/6617651C-4977-E411-9156-3417EBE2F493.root channels=tt isMC=1 nExtraJets=6 svFit=0 "
 # cmd = "./make_ntuples_cfg.py maxEvents=1000  outputFile=myTestFile.root useMiniAOD=1 inputFiles=file:///hdfs/store/mc/Phys14DR/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v2/00000/66E71D9C-9F77-E411-BFE9-00266CF9B184.root channels=tt isMC=1 nExtraJets=6 svFit=0 "
 
 cuts = "skimCuts=\""
@@ -21,8 +21,8 @@ cuts = cuts[0: len(cuts)-1] + "\""
 cmd += cuts
 tempFile = "do_test.sh"
 outFile = "do.sh"
-cmd = "submit_job.py SYNC make_ntuples_cfg.py channels=\"tt\" isMC=1 useMiniAOD=1 nExtraJets=6 svFit=0"
-cmd += " --campaign-tag=\"Phys14DR-PU20bx25*\" --das-replace-tuple=$fsa/MetaData/tuples/MiniAOD-13TeV.json --samples \"VBFHtt*\" -o %s" %tempFile
+cmd = "submit_job.py SYNC3 make_ntuples_cfg.py channels=\"tt\" isMC=1 useMiniAOD=1 nExtraJets=6 svFit=0"
+cmd += " --campaign-tag=\"Phys14DR-PU20bx25*\" --das-replace-tuple=$fsa/MetaData/tuples/MiniAOD-13TeV.json --samples \"VBF*\" -o %s" %tempFile
 os.system(cmd)
 
 
