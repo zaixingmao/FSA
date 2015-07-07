d52787beef2377fa5766f50aff611a4e7ec9d625
Zaixing <zaixing.mao@cern.ch>
2015-07-07 15:03:17 -0500

    updates

--- NtupleTools/test/submit_TT.py
+++ NtupleTools/test/submit_TT.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python
 import os
+import optparse
 
+
+def opts():
+    parser = optparse.OptionParser()
+    parser.add_option("--local", dest="runLocal", default=False, action="store_true", help="run local jobs")
+    parser.add_option("--doSVFit", dest="doSVFit", default=False, action="store_true", help="do SVFit")
+    parser.add_option("--singleJob", dest="singleJob", default=True, action="store_true", help="run 1 job")
+    parser.add_option("-o", dest="name", default='SYNC_745', help="")
+    parser.add_option("--sample", dest="sample", default='SUSY', help="")
+
+    options, args = parser.parse_args()
+
+    return options
+
+options = opts()
 
 skimCuts = {"ID": "object.tauID(\\\"decayModeFinding\\\") > 0.5 || object.tauID(\\\"decayModeFindingNewDMs\\\") > 0.5",
             "Pt": "object.pt() > 45",
             "Eta": "abs(object.eta()) < 2.1",
-
 #             "anti-Ele": "object.tauID(\\\"againstElectronVLooseMVA5\\\")>0.5",
 #             "anti-Mu": "object.tauID(\\\"againstMuonLoose3\\\")>0.5",
 #             "iso": "object.tauID(\\\"byCombinedIsolationDeltaBetaCorrRaw3Hits\\\")<10.0"
             }
 
+localJobInfo = {'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToBBHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/50000/1A81092B-1A11-E511-B683-002618943849.root",
+                'eventsToProcess': 'all',
+                'maxEvents': 1000}
 
-cmd = "./make_ntuples_cfg.py eventsToProcess=1:3050:304989  outputFile=myTestFile.root inputFiles=file:///hdfs/store/mc/Phys14DR/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v2/00000/D20C1E9C-9F77-E411-B8A7-00266CF9B184.root channels=tt isMC=1 nExtraJets=10 svFit=0 "
-# cmd = "./make_ntuples_cfg.py maxEvents=1000  outputFile=myTestFile.root useMiniAOD=1 inputFiles=file:///hdfs/store/mc/Phys14DR/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v2/00000/66E71D9C-9F77-E411-BFE9-00266CF9B184.root channels=tt isMC=1 nExtraJets=6 svFit=0 "
+
+SVFit = 1 if options.doSVFit else 0
+
+if ":" in localJobInfo['eventsToProcess']:
+    cmd = "./make_ntuples_cfg.py eventsToProcess=%s outputFile=myTestFile.root inputFiles=%s channels=tt isMC=1 nExtraJets=8 svFit=%i" %(localJobInfo['eventsToProcess'], localJobInfo['inputFile'], SVFit)
+else:
+    cmd = "./make_ntuples_cfg.py maxEvents=%i outputFile=myTestFile.root inputFiles=%s channels=tt isMC=1 nExtraJets=8 svFit=%i" %(localJobInfo['maxEvents'], localJobInfo['inputFile'], SVFit)
+
 
 cuts = "skimCuts=\""
 for ikey in skimCuts.keys():
@@ -21,24 +44,29 @@ for ikey in skimCuts.keys():
     cuts+= "%s," %(skimCuts[ikey].replace("object", "daughter(1)"))
 cuts = cuts[0: len(cuts)-1] + "\""
 cmd += cuts
-tempFile = "do_test.sh"
-outFile = "do.sh"
-cmd = "submit_job.py SYNC_745 make_ntuples_cfg.py channels=\"tt\" isMC=1 nExtraJets=8 svFit=0"
-cmd += " --campaign-tag=\"Phys14DR-PU20bx25*\" --das-replace-tuple=$fsa/MetaData/tuples/MiniAOD-13TeV_PHYS14DR.json --samples \"VBF*\" -o %s" %tempFile
+
+if not options.runLocal:
+    tempFile = "do_test.sh"
+    outFile = "do.sh"
+    cmd = "submit_job.py %s make_ntuples_cfg.py channels=\"tt\" isMC=1 nExtraJets=8 svFit=%i" %(options.name, SVFit)
+    cmd += " --campaign-tag=\"RunIISpring15DR74-Asympt25ns*\" --das-replace-tuple=$fsa/MetaData/tuples/MiniAOD-13TeV_RunIISpring15DR74.json --samples \"%s*\" -o %s" %(options.sample, tempFile)
 os.system(cmd)
 
+if not options.runLocal:
+    lines = open(tempFile, "r").readlines()
+    output = open(outFile, "w")
 
-lines = open(tempFile, "r").readlines()
-output = open(outFile, "w")
+    for i in range(0, len(lines)):
+        currentLine = lines[i]
+        if currentLine.find("svFit") != -1 and currentLine.find("farmoutAnalysisJobs") != -1:
+            newLine = currentLine[0:currentLine.find("farmoutAnalysisJobs")]
+            newLine += "farmoutAnalysisJobs --job-count=1 "
+            newLine += currentLine[currentLine.find("farmoutAnalysisJobs") + 19:currentLine.find("svFit")]
+            newLine += "svFit=%i %s" %(SVFit, cuts)
+            newLine += " 'inputFiles=$inputFileNames' 'outputFile=$outputFileName'"
+            currentLine = newLine
+        currentLine += '\n'
+        output.writelines(currentLine)
+    output.close()
 
-for i in range(0, len(lines)):
-    currentLine = lines[i]
-
-    if currentLine.find("svFit=1") != -1 and currentLine.find("farmoutAnalysisJobs") != -1:
-        currentLine = currentLine[0:currentLine.find("svFit=1")]
-        currentLine += "svFit=1 %s" %cuts
-        currentLine += "  'inputFiles=$inputFileNames' 'outputFile=$outputFileName'"
-    currentLine += '\n'
-    print currentLine
-    output.writelines(currentLine)
-output.close()
+print 'bash < do.sh'
