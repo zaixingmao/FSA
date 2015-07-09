8c9d0b3501f9a40ed345067b81da82ca2710607d
Edward Laird <elaird@gmail.com>
2015-07-09 10:38:21 -0500

    migrate content

--- DataFormats/interface/PATFinalStateFwd.h
+++ DataFormats/interface/PATFinalStateFwd.h
@@ -1,10 +1,1 @@
-#ifndef FinalStateAnalysis_DataFormats_PATFinalStateFwd_h
-#define FinalStateAnalysis_DataFormats_PATFinalStateFwd_h
-
-class PATFinalState;
-
-#include "FinalStateAnalysis/DataFormats/interface/FwdIncludes.h"
-#include "FinalStateAnalysis/DataFormats/interface/Macros.h"
-FWD_ABS_TYPEDEFS(PATFinalState)
-
-#endif /* end of include guard: FinalStateAnalysis_DataFormats_PATLe */
+PATFinalState.h
