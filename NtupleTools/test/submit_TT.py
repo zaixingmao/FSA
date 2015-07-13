bc2e508a087996d6d5eec9c349d1e0240aab3971
Zaixing <zaixing.mao@cern.ch>
2015-07-13 17:46:48 -0500

    config changes

--- NtupleTools/test/submit_TT.py
+++ NtupleTools/test/submit_TT.py
@@ -26,9 +26,8 @@ skimCuts = {"ID": "object.tauID(\\\"decayModeFindingNewDMs\\\") > 0.5",
 #             "iso": "object.tauID(\\\"byCombinedIsolationDeltaBetaCorrRaw3Hits\\\")<10.0"
             }
 
-localJobInfo = {'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToBBHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/
-50000/A0F69455-EF11-E511-A140-0CC47A4DEDE2.root",#"file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/10000/64BC29B9-5303-E511-8991-0CC47A4D99B0.root",
-                'eventsToProcess': 'all',#'1:1713:333892',
+localJobInfo = {'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/10000/2A3929AE-5303-E511-9EFE-0025905A48C0.root",
+                'eventsToProcess': '1:619:120614',
                 'maxEvents': -1}
 
 
