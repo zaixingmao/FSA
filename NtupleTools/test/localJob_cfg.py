0758c158ee4e47dec0b8685a05fc18eb0a04a296
Zaixing <zaixing.mao@cern.ch>
2015-07-17 11:20:40 -0500

    config changes

--- NtupleTools/test/localJob_cfg.py
+++ NtupleTools/test/localJob_cfg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-localJobInfo = {'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToBBHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/50000/A0F69455-EF11-E511-A140-0CC47A4DEDE2.root",
+localJobInfo = {'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/009D49A5-7314-E511-84EF-0025905A605E.root",
                 #'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/10000/64BC29B9-5303-E511-8991-0CC47A4D99B0.root",
-                'eventsToProcess': 'all',#'1:1713:333892',
+                'eventsToProcess': 'all',#'1:2113:411883', #run, lumi, event
                 'maxEvents': 5000}
