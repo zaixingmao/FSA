66ce52ca5813ab193c967376f354747fff5a2917
Zaixing <zaixing.mao@cern.ch>
2015-08-10 09:58:16 -0500

    config changes

--- NtupleTools/test/localJob_cfg.py
+++ NtupleTools/test/localJob_cfg.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-localJobInfo = {'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/1C36C8A2-8D14-E511-A059-0026189438D6.root",#"file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/10000/7AED07BC-5303-E511-8B68-00259074AE6A.root",
+localJobInfo = {'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/1C36C8A2-8D14-E511-A059-0026189438D6.root",
+#                'inputFile': "file:///hdfs/store/data/Run2015B/Tau/MINIAOD/PromptReco-v1/000/251/883/00000/F46D0932-492D-E511-9E48-02163E013417.root",
+#                'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/10000/2A3929AE-5303-E511-9EFE-0025905A48C0.root",
+                #"file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/10000/7AED07BC-5303-E511-8B68-00259074AE6A.root",
                 #'inputFile': "file:///hdfs/store/mc/RunIISpring15DR74/SUSYGluGluToHToTauTau_M-160_TuneCUETP8M1_13TeV-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/10000/64BC29B9-5303-E511-8991-0CC47A4D99B0.root",
-                'eventsToProcess': 'all',#'1:1318:256872', #run, lumi, event
-                'maxEvents': 50}
+                'eventsToProcess': 'all',#'1:10:1947',#'1:1318:256872', #run, lumi, event
+                'maxEvents':501}
