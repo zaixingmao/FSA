95f019a168dcb8a20302efc80fc96b038bee257b
Zaixing <zaixing.mao@cern.ch>
2015-11-16 18:25:09 -0600

    add trigger

--- NtupleTools/python/templates/muons.py
+++ NtupleTools/python/templates/muons.py
@@ -110,6 +110,7 @@ trigger = PSet(
     objectMu8El23 = 'matchToHLTFilter({object_idx}, "hltMu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVLMuonlegL3IsoFiltered8", 0.5)',
     objectIsoMu17 = 'matchToHLTFilter({object_idx}, "hltL3crIsoL1sSingleMu16erL1f0L2f10QL3f17QL3trkIsoFiltered0p09", 0.5)',
     objectIsoMu17hlt18 = 'matchToHLTFilter({object_idx}, "hltL3crIsoL1sSingleMu16erL1f0L2f10QL3f17QL3trkIsoFiltered0p09", 0.5, 18)',
+    objectIsoMu18 = 'matchToHLTFilter({object_idx}, "hltL3crIsoL1sMu16L1f0L2f10QL3f18QL3trkIsoFiltered0p09", 0.5)',
 
     objectIsoMu24 = 'matchToHLTFilter({object_idx}, "hltL3crIsoL1sMu20Eta2p1L1f0L2f10QL3f24QL3trkIsoFiltered0p09", 0.5)',
     objectIsoMu27 = 'matchToHLTFilter({object_idx}, "hltL3crIsoL1sMu25L1f0L2f10QL3f27QL3trkIsoFiltered0p09", 0.5)',
