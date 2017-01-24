3776d9ee7c054273217e6d1aa966a67865cafb6b
zaixingmao <zaixing.mao@cern.ch>
2017-01-24 16:00:31 -0600

    minor update

--- NtupleTools/test/localJob_cfg.py
+++ NtupleTools/test/localJob_cfg.py
@@ -8,7 +8,8 @@ import os
 #file = "/store/data/Run2016H/SingleMuon/MINIAOD/PromptReco-v2/000/281/207/00000/C01B8838-6282-E611-9884-02163E01414B.root"
 file = "/store/data/Run2016B/SingleElectron/MINIAOD/23Sep2016-v2/80000/08A02DC3-608C-E611-ADA5-0025905B85B6.root"
 
-file = "/store/data/Run2016H/SingleElectron/MINIAOD/PromptReco-v3/000/284/036/00000/1257A268-599F-E611-A437-02163E011E7A.root"
+file = "/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-200to400_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext2-v2/50000/08C3DBF9-8CD5-E611-BEF6-02163E00C762.root"
+file = "/store/user/anirban/vbf_zpm_tautau_M_zpm_1500/vbf_zpm_tautau_M_zpm_1500_step4/161209_190000/0000/vbf_zpm_tautau_M_zpm_1500_step4_1.root"
 #file = ""
 #for i in range(1, 51, 1):
 #    file += "/store/user/anirban/ZprimeToTauTau_M_2000_GEN_SIM/ZprimeToTauTau_M_2000_step4/160723_124341/0000/ZprimeToTauTau_M_2000_TuneCUETP8M1_tauola_13TeV_pythia8_step4_%i.root" %i
